a1060fac89eefa0e4019362519d7515ee3123edb
adirachmadi <55518700+adirachmadi@users.noreply.github.com>
2020-03-18 15:26:25 +0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(
+      Center(
+        child: Text(
+          """Biodata
+             Nama   : Rachmadi Dwima Gani 
+             TTL    : B.Lampung, 02-04-1999
+             NPM    : 1715061020
+             Hp     : 089610969683
+             email  : adirachmadi387@gmail.com
+             Alamat : JL.Danau singkarak no.31 Kedaton""" ,
+          textDirection: TextDirection.ltr,
+          style: TextStyle(fontSize: 15.0),
+        ),
+
+      )
+  );
+}
